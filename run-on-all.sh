2159bc584c392df3aed9f0e3256859bb50b0ac8b
Konstantin Baierer <konstantin.baierer@gmail.com>
2016-08-08 14:49:15 +0200

    Update datasets, cleaner structure for run-on-all.sh

--- run-on-all.sh
+++ run-on-all.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-set -ex
-DATASETS=$HOME/infolis-datasets/datasets
+DATASETS_ROOT=${DATASETS_ROOT:-$HOME/infolis-datasets/datasets}
+declare -A dataset_names=(
+    [replication-wiki]="$DATASETS_ROOT/replication-wiki/text/all"
+    [ssoar]="$DATASETS_ROOT/ssoar/text/all"
+    [mpra]="$DATASETS_ROOT/mpra/text/all"
+    [econstor]="$DATASETS_ROOT/econstor/text/all"
+    [econstor2000]="$DATASETS_ROOT/econstor/text/first2000"
+)
 declare -A filemeta=(
-    [$DATASETS/replication-wiki/text/all]="$DATASETS/replication-wiki/meta"
-    [$DATASETS/ssoar/text/all]="$DATASETS/ssoar/meta"
-    [$DATASETS/mpra/text/all]="$DATASETS/mpra/meta"
+    [$DATASETS_ROOT/replication-wiki/text/all]="$DATASETS_ROOT/replication-wiki/meta"
+    [$DATASETS_ROOT/ssoar/text/all]="$DATASETS_ROOT/ssoar/meta"
+    [$DATASETS_ROOT/mpra/text/all]="$DATASETS_ROOT/mpra/meta"
+    [$DATASETS_ROOT/econstor/text/all]="$DATASETS_ROOT/econstor/meta"
+    [$DATASETS_ROOT/econstor/text/first2000]="$DATASETS_ROOT/econstor/meta"
 )
 declare -A patternsets=(
     [dara]="import/dara-solr.json"
@@ -13,26 +21,48 @@ declare -A patternsets=(
 )
 
 timestamp() {
-    date -Iseconds | sed 's/[^0-9]//g'
+    date -Iseconds | sed 's/[^0-9]\+/-/g'
 }
 
 usage () {
-    echo "$(basename $0) <patternset>
+    echo "$(basename $0) <patternset> [dataset...]
 
         Patternsets: ${!patternsets[*]}
+
+        Datasets: ${!dataset_names[*]}
+
+        DATASETS_ROOT: $DATASETS_ROOT
     "
 }
-patternset="$1"
 
-if [[ -z "${patternsets[$patternset]}" ]];then
-    echo "no such patternset '$patternset'"
-    usage
-    exit 1
-fi
+main () {
+    declare -a datasets
+    patternset="$1"; shift;
+    if [[ -z "$patternset" || -z "${patternsets[$patternset]}" ]];then
+        echo "no such patternset '$patternset'"
+        usage
+        exit 1
+    fi
+    if [[ -z "$1" ]];then
+        datasets=("${!dataset_names[@]}")
+    else
+        datasets=("$@")
+    fi
+    local fm
+    for fm in "${datasets[@]}";do
+        if [[ -z "${dataset_names[$fm]}" ]];then
+            echo "No such dataset '${fm}'"
+            usage
+            exit 2
+        fi
+    done
+    local dataset textdir metadir outfile
+    for dataset in "${datasets[@]}";do
+        textdir="${dataset_names[$dataset]}"
+        metadir="${filemeta[$textdir]}"
+        outfile="output-${dataset}-${patternset}-$(timestamp).json"
+        ./dbminer.py search-patterns "${patternsets[$patternset]}" "$textdir" "$metadir" "$outfile"
+    done
+}
 
-
-for textdir in "${!filemeta[@]}";do
-    metadir="${filemeta[$textdir]}"
-    outfile="output-${patternset}-$(timestamp).json"
-    ./dbminer.py search-patterns "${patternsets[$patternset]}" "$textdir" "$metadir" "$outfile"
-done
+main "$@"
