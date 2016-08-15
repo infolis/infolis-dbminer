4119a3528fc4d872b4b1d423d2be38a91aa87540
Konstantin Baierer <konstantin.baierer@gmail.com>
2016-08-15 22:22:41 +0200

    Set resp. entityType for entities, merge-json command

--- dbminer.py
+++ dbminer.py
@@ -29,8 +29,6 @@ logging.basicConfig(level=logging.DEBUG,
     format='[%(levelname)s] %(asctime)s.%(msecs)03d - %(message)s',
     datefmt='%H:%M:%S')
 
-COUNTER = itertools.count()
-
 # save intermediate results every n files
 BAK_INTERVAL = 500
 
@@ -58,7 +56,7 @@ for x in range(0,100):
 CLEAR += '\r'
 
 xmlnode_text = lambda (x) : x.text
-urlescape = lambda (x) : re.sub("[^a-zA-Z0-9]", "-", x).lower()
+urlescape = lambda (x) : re.sub("[^a-zA-Z0-9]", "", re.sub("^https?://", '', x.lower()))
 
 RE_CACHE = {}
 def cachedRegex(r):
@@ -66,7 +64,7 @@ def cachedRegex(r):
         RE_CACHE[r] = re.compile(r)
     return RE_CACHE[r]
 
-def print_progress(cur, total, found, t0):
+def print_progress(cur, total, found, t0, idstr):
     """
     Show informative progress report on STDERR
     """
@@ -82,7 +80,9 @@ def print_progress(cur, total, found, t0):
             print_progress.eta = "%d:%02d:%02d" % (h, m, s)
         except AttributeErrror, e:
             print_progress.eta = "---"
-    sys.stderr.write("\r[%-2.2f%%] %-5d/%5d [found: %4d] [throughput: %4.3f/s] [ETA: %s]"%(
+    #  sys.stderr.write("\r[%-2.2f%%] %-5d/%5d [found: %4d] [throughput: %4.3f/s] [ETA: %s]"%(
+    sys.stderr.write("\r[%10s][%-2.2f%%] %-5d/%5d [found: %4d] [throughput: %4.3f/s] [ETA: %s]"%(
+        idstr,
         (cur * 1.0/total*100),
         cur,
         total,
@@ -104,10 +104,13 @@ def make_infolis_file_from_textfile(textfile, entity):
     Create an InfolisFile from a text file and the entity it manifests
     """
     infolis_file = {}
-    infolis_file['_id'] = 'file_' + str(COUNTER.next())
+    p, basename = os.path.split(textfile)
+    p, dir1 = os.path.split(p)
+    p, dir2 = os.path.split(p)
+    infolis_file['_id'] = 'file_%s_%s_%s' % (dir2, dir1, basename)
     infolis_file['fileStatus'] = 'AVAILABLE'
     infolis_file['mediaType'] = 'text/plain'
-    infolis_file['fileName'] = os.path.basename(textfile)
+    infolis_file['fileName'] = basename
     with open(textfile, 'r') as textin:
         textcontents = textin.read()
         infolis_file['md5'] = md5(textcontents).hexdigest()
@@ -127,17 +130,18 @@ def make_entity_from_oai(metafile):
     except AttributeError, e:
         entity['_id'] = 'entity_' + urlescape(root.find('.//dc:identifier', NS).text)
         entity['identifier'] = root.find('.//dc:identifier', NS).text
+    entity['entityType'] = 'publication'
     entity['name'] = root.find(".//dc:title", NS).text
     entity['authors'] = map(xmlnode_text, root.findall('.//dc:creator', NS))
     entity['subjects'] = map(xmlnode_text, root.findall('.//dc:subject', NS))
     try:
         entity['language'] = root.find('.//dc:language', NS).text
-    except:
-        logging.warn("%s has no language: %s"%(entity, e))
+    except AttributeError, e:
+        logging.warn("%s has no language: %s"%(entity['_id'], e))
     try:
         entity['abstractText'] = root.find('.//dc:description', NS).text
     except AttributeError, e:
-        logging.warn("%s has no abstractText: %s"%(entity, e))
+        logging.warn("%s has no abstractText: %s"%(entity['_id'], e))
 
     return entity
 
@@ -147,11 +151,22 @@ def make_entity_from_solr_doc(db, elem):
     """
     entity = {}
     _id = 'daradoc_' + elem.find(".//str[@name='id']").text
+    entity['entityType'] = 'dataset'
     entity['_id'] = _id
     entity['identifier'] = "http://www.da-ra.de/dara/search/search_show?res_id=%s&lang=en&detail=true"%(elem.find(".//str[@name='id']").text)
     entity['name'] = elem.findtext(".//arr[@name='title']/str")
     if 'Rezension' in entity['name'] \
-        or entity['name'].lower() in ['data', 'test', 'readme', 'contents', 'budget'] \
+        or entity['name'].lower() in [
+                'appendices',
+                'data',
+                "dataset",
+                'test',
+                'readme',
+                'tourismus',
+                'sportvereine'j,
+                'rezension',
+                'contents',
+                'budget'] \
         or re.match(cachedRegex("^Figure.*"), entity['name']) \
         or re.match(cachedRegex("^Table.*"), entity['name']):
         #  logging.warn("Skip this, not a dataset: %s" % entity['name'])
@@ -166,15 +181,17 @@ def make_entity_from_solr_doc(db, elem):
         entity['language'] = 'eng'
     return entity
 
-def make_entity_link_from_pattern(db, from_id, pat_id):
+def make_entity_link_from_pattern(indb, from_id, pat_id, outdb):
     """
     Create a link from an entity to another entity because of pattern
     """
-    ret = {}
-    infolisPattern = db['infolisPattern'][pat_id]
+    infolisPattern = indb['infolisPattern'][pat_id]
     conf = 1 / len(infolisPattern['linkTo'])
     for to_id in infolisPattern['linkTo']:
-        db['entityLink']['link_' + str(COUNTER.next())] = {
+        linkId = 'link_%s_%s' % (urlescape(from_id), urlescape(to_id))
+        outdb['entity'][from_id] = indb['entity'][from_id]
+        #  outdb['entity'][to_id] = indb['entity'][to_id]
+        outdb['entityLink'][linkId] = {
             'confidence': conf,
             'linkReason': infolisPattern['regexPattern'],
             'entityRelations': ['matches_pattern'],
@@ -202,7 +219,7 @@ def make_pattern(db, prefix, title, _id):
         'linkTo': [_id]
     }
     # --------------------------
-    # 2) Up to first comma, if result contains a space
+    # 2) Up to first comma, if after comma only numbers and minus and if result contains a space
     # --------------------------
     secondform = re.sub(",[-\s0-9]+", "", title)
     if secondform != title and re.search(' ', secondform):
@@ -232,9 +249,11 @@ def make_pattern(db, prefix, title, _id):
 #{{{
 
 def search_patterns_in_files(dbfile, textfiles, metadir):
-    bakfile = "/tmp/" + urlescape(dbfile) +"_"+ urlescape(metadir) + ".json"
+    bakfile = "/tmp/" + urlescape(dbfile) + "_" + urlescape(metadir) + ".json"
+    idstr = re.sub(".*/", "", dbfile) + '_' + re.sub(".*/", "", re.sub("/meta$", "", metadir))
+    outdb = {'entity':{}, 'entityLink':{}}
     with open(dbfile) as jsoninfile:
-        db = json.load(jsoninfile)
+        indb = json.load(jsoninfile)
         cur = 0
         total = len(textfiles)
         total_found = 0
@@ -245,10 +264,10 @@ def search_patterns_in_files(dbfile, textfiles, metadir):
             found = []
             metafile = metadir + "/" + os.path.splitext(os.path.basename(textfile))[0] + ".xml"
             entity = make_entity_from_oai(metafile)
-            db['entity'][entity['_id']] = entity
+            indb['entity'][entity['_id']] = entity
             infolis_file, textcontents = make_infolis_file_from_textfile(textfile, entity)
-            for pat_id in db['infolisPattern']:
-                pat = db['infolisPattern'][pat_id]
+            for pat_id in indb['infolisPattern']:
+                pat = indb['infolisPattern'][pat_id]
                 #
                 # XXX SEARCH IS HERE
                 #
@@ -257,16 +276,16 @@ def search_patterns_in_files(dbfile, textfiles, metadir):
                     continue
                 if re.search(cachedRegex(pat['regexPattern']), textcontents):
                     found.append(pat_id)
-                    make_entity_link_from_pattern(db, entity['_id'], pat_id)
+                    make_entity_link_from_pattern(indb, entity['_id'], pat_id, outdb)
             cur += 1
             #  sys.stderr.write(CLEAR)
-            print_progress(cur, total, total_found, t0)
+            print_progress(cur, total, total_found, t0, idstr)
             total_found += len(found)
             if cur % BAK_INTERVAL == 0:
                 logging.debug("Saving intermediary results to %s" % bakfile)
                 with open(bakfile, 'w') as jsonoutfile:
-                    jsonoutfile.write(json.dumps(db, indent=2))
-    return db
+                    jsonoutfile.write(json.dumps(outdb, indent=2))
+    return outdb
 
 #}}}
 
@@ -300,7 +319,7 @@ def jsonify_dara(darafile, outdbfile):
                 if '_doi' in entity and entity['_doi'] != None:
                     found += make_pattern(db, 'darapat', entity['_doi'], entity['_id'])
             cur += 1
-            print_progress(cur, total, found, t0)
+            print_progress(cur, total, found, t0, 'import-dara')
             elem.clear()
         if elem.getparent() is None:
             break
@@ -326,6 +345,7 @@ def jsonify_databases(infile, outfile):
             url = row[DATABASES_CSV_HEADER['URL']]
             keywords = row[DATABASES_CSV_HEADER['KEYWORDS']].split(" ; ")
             db['entity'][_id] = {
+                "entityType": 'database',
                 "url": url,
                 "identifier": url,
                 "name": title
@@ -334,7 +354,7 @@ def jsonify_databases(infile, outfile):
             for pat in keywords:
                 if pat != '':
                     found += make_pattern(db, 'dbpat', pat, _id)
-            print_progress(cur, total, found, t0)
+            print_progress(cur, total, found, t0, 'import-db')
         with open(outfile, mode="w") as jsonfile:
             jsonfile.write(json.dumps(db, indent=2))
 
@@ -353,14 +373,53 @@ def jsonify_icpsr_studies(infile, outfile):
             doi = row[ICPSRSTUDIES_CSV_HEADER['DOI']]
             db['entity'][_id] = {
                 'name': title,
+                'entityType': 'dataset',
                 'identifier': doi }
             found += make_pattern(db, 'icpsrpat', title, _id)
             found += make_pattern(db, 'icpsrpat', doi, _id)
             cur += 1
-            print_progress(cur, total, found, t0)
+            print_progress(cur, total, found, t0, 'import-icpsr')
         with open(outfile, mode="w") as jsonfile:
             jsonfile.write(json.dumps(db, indent=2, encoding='latin1'))
+
+# http://stackoverflow.com/a/15836901/201318
+class MergeError(Exception):
     pass
+def data_merge(a, b):
+    """merges b into a and return merged result
+
+    NOTE: tuples and arbitrary objects are not handled as it is totally ambiguous what should happen"""
+    key = None
+    # ## debug output
+    # sys.stderr.write("DEBUG: %s to %s\n" %(b,a))
+    try:
+        if a is None or isinstance(a, str) or isinstance(a, unicode) or isinstance(a, int) or isinstance(a, long) or isinstance(a, float):
+            # border case for first run or if a is a primitive
+            a = b
+        elif isinstance(a, list):
+            # lists can be only appended
+            if isinstance(b, list):
+                # merge lists
+                a.extend(b)
+            else:
+                # append to list
+                a.append(b)
+        elif isinstance(a, dict):
+            # dicts must be merged
+            if isinstance(b, dict):
+                for key in b:
+                    if key in a:
+                        a[key] = data_merge(a[key], b[key])
+                    else:
+                        a[key] = b[key]
+            else:
+                raise MergeError('Cannot merge non-dict "%s" into dict "%s"' % (b, a))
+        else:
+            raise MergeError('NOT IMPLEMENTED "%s" into "%s"' % (b, a))
+    except TypeError, e:
+        raise MergeError('TypeError "%s" in key "%s" when merging "%s" into "%s"' % (e, key, b, a))
+    return a
+
 #}}}
 
 
@@ -383,7 +442,7 @@ def print_usage(exit_code):
     jsonify-icpsr-studies <csv> <out-json>
         Convert ICPSR studies CSV to JSON
 
-    TODO merge-dbs <outjson> <in1> <in2...>
+    merge-json <outjson> <in1> <in2...>
         Merges JSON files to be uploaded or used for search
 
     search-patterns <db> <textdir> <metadir> <outdb>
@@ -414,6 +473,19 @@ if __name__ == "__main__":
         if len(sys.argv) != 6:
             print_usage(1)
         search_patterns(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    elif cmd == 'merge-json':
+        if len(sys.argv) < 5:
+            print_usage(1)
+        outname = sys.argv[2]
+        outjson = {}
+        for inname in sys.argv[3:]:
+            with open(inname, 'r') as infile:
+                logging.debug('Merging %s' % inname)
+                injson = json.load(infile)
+                outjson = data_merge(outjson, injson)
+        with open(outname, 'w') as outfile:
+            logging.debug("Writing to %s" % outname)
+            json.dump(outjson, outfile)
     else:
         print_usage(1)
 #}}}
